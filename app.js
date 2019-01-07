f379e389e0be40509e0cb35b00e5fe7d2c656c6d
fopineda <fopineda95@gmail.com>
2019-01-07 14:12:58 -0500

    using yargs 15:15

--- app.js
+++ app.js
@@ -3,7 +3,23 @@ console.log('Starting app.js');
 const fs = require('fs');
 const _ = require('lodash'); 
 const notes = require('./notes.js');
+const yargs = require("yargs");
 
 
+const argv = yargs.argv;
+var command = process.argv[2];
+console.log("Command: " + command);
+console.log("yargs: "+ argv);
 
 
+if (command === 'add'){
+    notes.addNote(argv.title, argv.body);
+} else if (command  === "list"){
+    notes.getAll();
+} else if (command  === "read"){
+    notes.getNote(argv.title);
+} else if (command  === "remove"){
+    notes.removeNote(argv.title);
+} else{
+    console.log("command not recognized")
+}
--- notes.js
+++ notes.js
@@ -1,7 +1,25 @@
 console.log('Starting notes.js');
 // console.log(module);
 
-module.exports.addNote = () => {
-    console.log('addNote');
-    return 'New note';
+var addNote = (title, body) => {
+    console.log('Adding note', title, body);
 };
+
+var getAll = () => {
+    console.log('Getting all notes')
+}
+
+var getNote = (title) => {
+    console.log(title);
+}
+
+var removeNote = () => {
+    console.log(title);
+}
+
+module.exports = {
+    addNote,
+    getAll,
+    getNote,
+    removeNote
+}
