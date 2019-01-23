1bfcdacea4e9657e1e2b814dcac488b32c8839d6
fopineda <fopineda95@gmail.com>
2019-01-23 14:48:34 -0500

    added debugging in node notes in playground folder, actually

--- playground/debugging.js
+++ playground/debugging.js
@@ -9,3 +9,11 @@ debugger;
 person.name = "Osiel";
 
 console.log(person);
+
+// node inspect debugging.js
+// > n (next)
+// > c (contine to end or where "debugger;" is located)
+// > repl (allows to manipulate or type out variables from where it's currently standing)
+// CTRL + C to exit repl
+// CTRL + C (x2) to exit debug
+// adding "debugger;" to code is the same thing as addding a breakpoint, you will type "c" to get there.
